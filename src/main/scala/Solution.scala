9bce7b53fd940d7385bd77abad03ca21d580f9c4
Waldemar Wosiński <kastoestoramadus@gmail.com>
2016-01-10 21:14:47 +0100

    preparations

---
+++ src/main/scala/Solution.scala
@@ -0,0 +1,12 @@
+
+object Solution {
+  def howManySqrts(A: Int, B: Int): Int = ???
+
+  def balance(chars: List[Char]): Boolean = ???
+
+  def solution(A: Array[Int]): Int = ???
+
+  type PizzaRequest = AnyRef // FIXME make case class
+
+  def shortestPizzaWaiting(howMany: Int, orders: Iterator[PizzaRequest]) = ???
+}
---
+++ src/test/scala/PizzaOptimizerSpec.scala
@@ -0,0 +1,19 @@
+import java.io.File
+
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class PizzaOptimizerSpec extends FlatSpec with Matchers {
+/*
+  "pizza optimizer" should "pass input #01" in {
+    val input = List(PizzaRequest(0, 3),PizzaRequest(1, 9),PizzaRequest(2, 5)).iterator
+    new PizzaWaitingOptimizer(3, input)
+      .minAvg should equal(8)
+  }
+  "pizza optimizer" should "pass input #00" in {
+    val input = List(PizzaRequest(0, 3),PizzaRequest(1, 9),PizzaRequest(2, 6)).iterator
+    new PizzaWaitingOptimizer(3, input)
+      .minAvg should equal(9)
+  }
+  */
+}
